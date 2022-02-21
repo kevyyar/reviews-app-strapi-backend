ad64f4e6267d727b65db5b9b2f327ee7e7dcdabd
Kevin Barreto <barretokevin@hotmail.com>
2022-02-21 17:17:40 -0600

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '63707cdf39a666d6ed223f354c265370'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cce23176-37bf-4171-8a4b-71fda990afcd'
+};
